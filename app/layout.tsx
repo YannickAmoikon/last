ded54f0f6d4b8e79853ccf9b7500430e311b7518
macbook <amoikon44@gmail.com>
2024-10-23 03:18:45 +0000

    Thanks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 // app/layout.tsx
-import {Inter} from "next/font/google";
 import "@/styles/globals.css";
 import AuthProvider from "@/components/providers/auth-provider";
 import ReduxProvider from "@/components/providers/redux-provider";
+import LocalFont from 'next/font/local'
 
-const inter = Inter({subsets: ["latin"]});
+const font = LocalFont({
+  src: '../fonts/GeistVF.woff',
+  variable: '--font-geist',
+  weight: '100 900', // Ceci permet d'utiliser tous les poids de 100 à 900
+})
+
+const fontMono = LocalFont({
+  src: '../fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900', // Même chose pour la version mono
+})
 
 export default function RootLayout({
                                      children,
@@ -13,9 +23,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-    <body>
-    <AuthProvider>
-      <ReduxProvider>
+    <body
+      className={`${font.variable} ${fontMono.variable} antialiased font-normal`}
+      
+    >
+      <AuthProvider>
+        <ReduxProvider>
         {children}
       </ReduxProvider>
     </AuthProvider>
